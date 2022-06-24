37fc783501f5c676ed1eada4185b4360d39ff66b
Janista Sihirunwong <janista.shrw@gmail.com>
2022-06-24 17:56:48 +0700

    janista: done tailWind homepage & waiting pAe to decorate

--- frontend/src/components/userComp/homeDetail/HomeDetail.js
+++ frontend/src/components/userComp/homeDetail/HomeDetail.js
@@ -24,10 +24,9 @@ const HomeDetail = () => {
 
   useEffect(() => {
     //console.log("test", pageCat);
-    console.log();
     const initFunc = async () => {
       let result = await axios.get(`/users/home/${pageCat}`);
-      console.log(result);
+      // console.log(result);
       if (result.status === 200) {
         dispatch(initHome(result.data));
       }
@@ -94,92 +93,119 @@ const HomeDetail = () => {
     //       </div>
     //     ))}
     // </div>
-    <div>
+    <div style={{ paddingTop: "100px", padding: "10px" }}>
       {state.order &&
         state.order.map((x, index) => (
-          <div className="mt-5 sm:mt-0">
-            <div className="md:grid md:grid-cols-3 md:gap-6">
-              <div className="mt-5 md:mt-0 md:col-span-2">
-                <form action="#" method="POST">
+          <div className="mt-2 sm:mt-0">
+            <div className="md:grid md:grid-cols-1 md:gap-1">
+              <div className="mt-2 md:mt-0 md:col-span-2">
+                <form action="#">
                   <div className="shadow overflow-hidden sm:rounded-md">
-                    <div className="px-4 py-5 bg-home space-y-6 sm:p-6">
+                    <div className="px-4 py-5 bg-home space-y-2 sm:p-6">
                       <fieldset>
-                        <legend className="contents text-base font-medium text-white">
+                        <legend className="contents text-xl font-large text-white uppercase">
                           {x.product_name}
                         </legend>
-                        <img
-                          width="100px"
-                          height="100px"
-                          src={`${x.product_photo}`}
-                        ></img>
 
-                        <div className="mt-4 space-y-4">
-                          <div className="flex items-center">
-                            {x.hot_price ? (
-                              <>
-                                <input
-                                  id="push-everything"
-                                  name="type"
-                                  type="radio"
-                                  className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                                />
-                                <label
-                                  htmlFor="hot"
-                                  className="ml-3 block text-sm font-medium text-white"
-                                >
-                                  HOT {x.hot_price} BAHT
-                                </label>
-                              </>
-                            ) : (
-                              <></>
-                            )}
-                          </div>
+                        <table>
+                          <tr>
+                            <td>
+                              <img
+                                style={{ margin: "30px", borderRadius: "10px" }}
+                                width="100px"
+                                height="100px"
+                                src={`${x.product_photo}`}
+                              ></img>
+                            </td>
+                            <td>
+                              <div className="mt-4 space-y-4">
+                                <div className="flex items-center">
+                                  {x.hot_price ? (
+                                    <>
+                                      <input
+                                        id="push-everything"
+                                        name="type"
+                                        type="radio"
+                                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+                                      />
+                                      <label
+                                        htmlFor="hot"
+                                        className="ml-3 block text-sm font-medium text-white"
+                                      >
+                                        HOT {x.hot_price} BAHT
+                                      </label>
+                                    </>
+                                  ) : (
+                                    <></>
+                                  )}
+                                </div>
+                                <div className="flex items-center">
+                                  {x.iced_price ? (
+                                    <>
+                                      <input
+                                        id="push-everything"
+                                        name="type"
+                                        type="radio"
+                                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+                                      />
+                                      <label
+                                        htmlFor="iced"
+                                        className="ml-3 block text-sm font-medium text-white"
+                                      >
+                                        ICED {x.iced_price} BAHT
+                                      </label>
+                                    </>
+                                  ) : (
+                                    <></>
+                                  )}
+                                </div>
+                                <div className="flex items-center">
+                                  {x.frappe_price ? (
+                                    <>
+                                      <input
+                                        id="push-everything"
+                                        name="type"
+                                        type="radio"
+                                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+                                      />
+                                      <label
+                                        htmlFor="frappe"
+                                        className="ml-3 block text-sm font-medium text-white"
+                                      >
+                                        FRAPPE {x.frappe_price} BAHT
+                                      </label>
+                                    </>
+                                  ) : (
+                                    <></>
+                                  )}
+                                </div>
 
-                          <div className="flex items-center">
-                            {x.iced_price ? (
-                              <>
-                                <input
-                                  id="push-everything"
-                                  name="type"
-                                  type="radio"
-                                  className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                                />
-                                <label
-                                  htmlFor="iced"
-                                  className="ml-3 block text-sm font-medium text-white"
+                                <button
+                                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-800 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                  onClick={() =>
+                                    dispatch(decreaseMenuOrder(index))
+                                  }
                                 >
-                                  ICED {x.iced_price} BAHT
-                                </label>
-                              </>
-                            ) : (
-                              <></>
-                            )}
-                          </div>
+                                  -
+                                </button>
+                                <p style={{ color: "white" }}>{x.quantity}</p>
+                                {console.log(x.quantity)}
 
-                          <div className="flex items-center">
-                            {x.frappe_price ? (
-                              <>
-                                <input
-                                  id="push-everything"
-                                  name="type"
-                                  type="radio"
-                                  className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                                />
-                                <label
-                                  htmlFor="frappe"
-                                  className="ml-3 block text-sm font-medium text-white"
+                                <button
+                                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-800 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                  onClick={() =>
+                                    dispatch(increaseMenuOrder(index))
+                                  }
                                 >
-                                  FRAPPE {x.frappe_price} BAHT
-                                </label>
-                              </>
-                            ) : (
-                              <></>
-                            )}
-                          </div>
-                        </div>
+                                  +
+                                </button>
+                              </div>
+                            </td>
+                          </tr>
+                        </table>
                       </fieldset>
                     </div>
-                    <div className="px-4 py-3 bg-home text-right sm:px-6">
+                    <div className="px-4 py-3 bg-home text-center sm:px-6">
                       <button
                         type="submit"
                         className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-800 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
