05ac829c0d7ded3306ee2a64a3f88e87f7618de7
MewPk <milzaza123@gmail.com>
2022-06-26 23:39:00 +0700

    mew: init line liff login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,40 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import LayoutComp from "./components/userComp/layout/LayoutComp";
 import HomeDetail from "./components/userComp/homeDetail/HomeDetail";
 import EditProduct from "./components/userComp/editDetail/editProduct";
 import axios from "axios";
 
+
+
 function App() {
   axios.defaults.baseURL = "http://localhost:3000";
+
+  const liff = window.liff;
+  const liffid = "1657099044-Jjo1LZmv";
+  const [data,setData] = useState(null)
+  const initLine = async () => {
+    await liff.init({ liffId: `${liffid}` }).catch((err) => {
+      throw err;
+    });
+    if (liff.isLoggedIn()&& !data) {
+      let getProfile = await liff.getProfile();
+      setData({
+        name: getProfile.displayName,
+        userLineID: getProfile.userId,
+        pictureUrl: getProfile.pictureUrl,
+      });
+    } else {
+      liff.login();
+    }
+  };
+
+  initLine();
+
+  
+  console.log(data);
+
   return (
     <div>
       <Routes>
